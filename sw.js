14eae371666b19bd00ac9e5646924e579048d523
glennmueller9-design <glenn.mueller9@icloud.com>
2026-05-31 17:39:00 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='purzelhuus-menu-v12';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./data.js','./manifest.webmanifest','./icon.svg','./purzelhuus-logo.jpeg'])))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
